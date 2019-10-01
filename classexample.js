126be2f7c96aba87bd523eafc98c97d2842d5454
dskog <darrell.skogman@hallco.org>
2019-10-01 11:17:50 -0400

    sdfa

---
+++ classexample.js
@@ -0,0 +1,7 @@
+var numOne = 104;
+var numTwo = 102;
+var numThree = 103;
+console.log("numOne + numTwo: " + (numOne + numTwo));
+console.log("numTwo - numThree: " + (numTwo - numThree));
+console.log("numTwo * numOne: " + (numTwo*numOne));
+console.log("numOne / numTwo: " + (numOne/numTwo));
