689d5bfd454c01f6c68fb90745e74029cc4db194
chsprogrammer <darrell.skogman@hallco.org>
2019-10-02 09:57:17 -0400

    Code for 10022019
    login screen starter code

---
+++ loginScreen.js
@@ -0,0 +1,12 @@
+function verifyInput(){
+    var username = "my name";
+    var password = "password";
+    if(document.getElementById('uName').value==username)
+    {
+        document.getElementById("response").innerHTML = "Welcome, " + username;
+    }
+    //add code here if the username is bad
+
+    //modify the code above to check the password as well as the username
+    //and return appropriate messages
+}
