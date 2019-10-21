bb5f2a2b9aeb3a2a271d5a408a5c354838e61dc3
dskogman <darrell.skogman@hallco.org>
2019-10-21 11:01:43 -0400

    in-class changes

--- scripts/oct21scripttemplate.js
+++ scripts/oct21scripttemplate.js
@@ -1,12 +1,22 @@
 //create a main function
-
+var main = function(){
     //create a variable for a function addCommentFromInputBox
+    var addCommentFromInputBox = function(){
         //create a variable for new comments
+        var $new_comment;
 
         //check to see if the input box ISN'T empty
+        if ($("comment-input input").val() !== ""){
             //if it isn't, store the stuff in the input box as text inside a new paragraph
+            $new_comment=$("<p>").text($(".comment-input input").val());
             //append the new comment to the comments section
-
+            $(".comments").append($new_comment);
+        }
+    };
     //create an event handler: when comment input button is clicked then call the addCommentFunction
+    $(".comment-input button").on("click", function (event){addCommentFromInputBox()
+    });
+};
 
 //call the main function
+$(document).ready(main);
