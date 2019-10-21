be06111a7f36e35975d8b54c7681d789c2ef194c
dskogman <darrell.skogman@hallco.org>
2019-10-21 10:20:32 -0400

    added oct21 files

---
+++ scripts/oct21scripttemplate.js
@@ -0,0 +1,12 @@
+//create a main function
+
+    //create a variable for a function addCommentFromInputBox
+        //create a variable for new comments
+
+        //check to see if the input box ISN'T empty
+            //if it isn't, store the stuff in the input box as text inside a new paragraph
+            //append the new comment to the comments section
+
+    //create an event handler: when comment input button is clicked then call the addCommentFunction
+
+//call the main function
